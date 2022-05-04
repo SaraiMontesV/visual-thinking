7e695d71dd27881eb2bcee6586470b92e5980df7
SaraiMV <sarizvargas@gmail.com>
2022-05-03 20:18:34 -0500

    Running linter

--- test/controllers/PartnerController.test.js
+++ test/controllers/PartnerController.test.js
@@ -1,42 +1,42 @@
 const PartnerController = require("./../../lib/controllers/PartnerController");
 
 describe("Unit Test for PartnersController", () => {
-  test("1) Validate length of students", () => {
-    const allStudents = PartnerController.getAllStudents();
-    expect(allStudents.length).toBeGreaterThanOrEqual(0);
-  });
+    test("1) Validate length of students", () => {
+        const allStudents = PartnerController.getAllStudents();
+        expect(allStudents.length).toBeGreaterThanOrEqual(0);
+    });
 
-  test("2) Verifying properties of students ", () => {
-    const student = [
-      {
-        id: "6264d5d864550a6a867d8e22",
-        name: "Rosanne",
-        email: "Juliette@visualpartnership.xyz",
-        credits: 469,
-        enrollments: ["Visual Thinking Avanzado"],
-        previousCourses: 3,
-        haveCertification: true,
-      },
-    ];
-    expect(student[0]).toEqual(
-      expect.objectContaining({
-        id: expect.any(String),
-        name: expect.any(String),
-        email: expect.any(String),
-        credits: expect.any(Number),
-        haveCertification: expect.any(Boolean),
-        previousCourses: expect.any(Number),
-      })
-    );
-  });
+    test("2) Verifying properties of students ", () => {
+        const student = [
+            {
+                id: "6264d5d864550a6a867d8e22",
+                name: "Rosanne",
+                email: "Juliette@visualpartnership.xyz",
+                credits: 469,
+                enrollments: ["Visual Thinking Avanzado"],
+                previousCourses: 3,
+                haveCertification: true,
+            },
+        ];
+        expect(student[0]).toEqual(
+            expect.objectContaining({
+                id: expect.any(String),
+                name: expect.any(String),
+                email: expect.any(String),
+                credits: expect.any(Number),
+                haveCertification: expect.any(Boolean),
+                previousCourses: expect.any(Number),
+            })
+        );
+    });
 
-  test("3) Get all emails not to be null", () => {
-    const allEmails = PartnerController.getEmailsWithCertification();
-    expect(allEmails).not.toBeNull();
-  });
+    test("3) Get all emails not to be null", () => {
+        const allEmails = PartnerController.getEmailsWithCertification();
+        expect(allEmails).not.toBeNull();
+    });
 
-  test("4) Get students with credits up to 500", () => {
-    const creditsUpTo500 = PartnerController.getCreditsUpTo500();
-    expect(creditsUpTo500).not.toBeNull();
-  });
+    test("4) Get students with credits up to 500", () => {
+        const creditsUpTo500 = PartnerController.getCreditsUpTo500();
+        expect(creditsUpTo500).not.toBeNull();
+    });
 });
--- test/services/PartnersService.test.js
+++ test/services/PartnersService.test.js
@@ -3,31 +3,31 @@ const Reader = require("../../lib/utils/Reader");
 const students = Reader.readJsonFile("./test/test_visualpartners.json");
 
 describe("Unit test for Partner Service", () => {
-  test("1) Get all students not to be null", () => {
-    const allStudents = PartnersService.getAllStudents(students);
-    expect(allStudents).not.toBeNull();
-  });
+    test("1) Get all students not to be null", () => {
+        const allStudents = PartnersService.getAllStudents(students);
+        expect(allStudents).not.toBeNull();
+    });
 
-  test("2) Verifying properties of students ", () => {
-    expect(students[0]).toEqual(
-      expect.objectContaining({
-        id: expect.any(String),
-        name: expect.any(String),
-        email: expect.any(String),
-        credits: expect.any(Number),
-        haveCertification: expect.any(Boolean),
-        previousCourses: expect.any(Number),
-      })
-    );
-  });
+    test("2) Verifying properties of students ", () => {
+        expect(students[0]).toEqual(
+            expect.objectContaining({
+                id: expect.any(String),
+                name: expect.any(String),
+                email: expect.any(String),
+                credits: expect.any(Number),
+                haveCertification: expect.any(Boolean),
+                previousCourses: expect.any(Number),
+            })
+        );
+    });
 
-  test("3) Get all emails not to be null", () => {
-    const allEmails = PartnersService.getEmailsWithCertification(students);
-    expect(allEmails).not.toBeNull();
-  });
+    test("3) Get all emails not to be null", () => {
+        const allEmails = PartnersService.getEmailsWithCertification(students);
+        expect(allEmails).not.toBeNull();
+    });
 
-  test("4) Get students with credits up to 500", () => {
-    const creditsUpTo500 = PartnersService.getCreditsUpTo500(students);
-    expect(creditsUpTo500).not.toBeNull();
-  });
+    test("4) Get students with credits up to 500", () => {
+        const creditsUpTo500 = PartnersService.getCreditsUpTo500(students);
+        expect(creditsUpTo500).not.toBeNull();
+    });
 });
